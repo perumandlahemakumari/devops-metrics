44ec1e2aec1a5e60b57e9df5b300a9eb64a5dcb6
perumandlahemakumari <160330605+perumandlahemakumari@users.noreply.github.com>
2025-02-08 17:48:24 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,18 @@
-ECHO is off.
+from flask import Flask, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/metrics')
+def get_metrics():
+    conn = psycopg2.connect("dbname=devops user=admin password=pass host=postgres")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM metrics ORDER BY timestamp DESC LIMIT 10;")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
